c2b4a93866388b141ee1e2aa5b33df25a87f0516
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2021-11-03 20:36:06 +0000

    added order products to orders admin page

--- order/admin.py
+++ order/admin.py
@@ -22,7 +22,7 @@ class OrderAdmin(admin.ModelAdmin):
     ]
 
     def order_products(self, obj):
-        return ', '.join(['{}  {}  {}'.format(child.item.name, child.size, child.quantity) for child in obj.order_products.all()])
+        return ', '.join(['{}  {}  {}'.format(child.item.name, child.size, str(child.quantity)) for child in obj.order_products.all()])
 
     order_products.short_description = "Products orderd"
 
