9fbc02b5b3c04a515ae39bf83f2f4b5ed1b355dc
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2022-07-14 18:52:40 +0100

    added enquaries app admin

--- enquaries/admin.py
+++ enquaries/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Enquiry
 
-# Register your models here.
+
+@admin.register(Enquiry)
+class EnquaryAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "name",
+        "description",
+        "user",
+        "date_added",
+        "date_found",
+        "status",
+    ]
