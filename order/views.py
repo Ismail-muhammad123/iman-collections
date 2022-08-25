546410273c57a71fbb268fbe74bf4a09b47be40f
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2022-08-26 00:45:07 +0100

    done

--- order/views.py
+++ order/views.py
@@ -1,10 +1,10 @@
 import datetime
-import imp
 from django.shortcuts import render, redirect
 from products.models import Product
 from django.shortcuts import get_object_or_404
 from .models import Order
 from django.http import Http404
+from django.urls import reverse
 
 
 # Create your views here.
@@ -45,16 +45,21 @@ def new_order(request):
 
 
 def my_order(request):
-    return render(request, template_name='order/order.html')
+    order_id = request.GET.get("order_id", None)
+
+    if order_id is not None:
+        return redirect(reverse('track_order', args=[order_id]))
+    else:
+        return render(request, template_name='order/order.html')
 
 
 def track_order(request, tracking_id):
     id = eval(tracking_id)
     try:
         order = Order.objects.get(id=id)
-    except Order.DoesNotExist:
-        raise Http404
-    delivery_date = order.payment.payed_at + \
-        datetime.timedelta(days=order.product.delivery_days)
+        delivery_date = order.payment.payed_at + \
+            datetime.timedelta(days=order.product.delivery_days)
 
-    return render(request, template_name='order/order.html', context={"order": order, "delivery_date": delivery_date})
+        return render(request, template_name='order/order.html', context={"order": order, "delivery_date": delivery_date})
+    except:
+        raise Http404
