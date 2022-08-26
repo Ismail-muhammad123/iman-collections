7c4741d688ba466fa0ecb89cfa85d5053253d04d
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2022-08-26 01:04:40 +0100

    limited the number of categories at index page

--- base/views.py
+++ base/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from products.models import Product, Category
 
 
-
 def index(request):
     products = Product.objects.all()[0:8]
-    categories = Category.objects.all()
+    categories = Category.objects.all()[:5]
     return render(request, 'base/index.html', context={"products": products, "categories": categories})
